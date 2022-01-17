9dd5256423c4490eef419a25eef2bb7daef5ff34
LucasCav <lucas.oklp@gmail.com>
2022-01-17 14:43:53 -0300

    foram adiconadas box-shadow aos items

--- index.js
+++ index.js
@@ -13,17 +13,17 @@ app.use(morgan("tiny"));
 
 app.use(cors());
 
-
 app.use(express.json());
-require('dotenv').config();
+require("dotenv").config();
 
+mongoose
+  .connect("", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("database conectado"))
+  .catch((err) => console.log("database fail", err));
 
-mongoose.connect("mongodb+srv://LucasBanco:12065722@cluster0.kuddv.mongodb.net/oneSigntPro?authSource=admin&replicaSet=atlas-q4t055-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true",{
-    useNewUrlParser:true,
-    useUnifiedTopology: true
-}) 
-.then(() => console.log("database conectado"))
-.catch((err) => console.log("database fail", err));
-
-
-app.listen(port,()=> {console.log("8080 connectado")});
+app.listen(port, () => {
+  console.log("8080 connectado");
+});
