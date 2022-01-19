dd1f51bf22f0089ddf323f6653f3308d69431bb3
LucasCav <lucas.oklp@gmail.com>
2022-01-19 19:03:40 -0300

    Adicionando media queries aos items

--- client/src/todos/ItemTodo.js
+++ client/src/todos/ItemTodo.js
@@ -5,7 +5,7 @@ import { RiDeleteBin5Line } from "react-icons/ri";
 
 import "./todo.css";
 
-const ItemTodo = ({ todo,deleteNote }) => {
+const ItemTodo = ({ todo, deleteNote }) => {
   const [onUpdate, setOnUpdate] = useState(false);
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
@@ -20,48 +20,48 @@ const ItemTodo = ({ todo,deleteNote }) => {
       .then((res) => {})
       .catch((err) => console.log(err));
   };
-    return (
-      <>
-        {!onUpdate ? (
-          <div className="item-todo">
-            <h4>{todo.title}</h4>
-            <h4>{todo.description}</h4>
-            <RiDeleteBin5Line
-              className="delete-icon"
-              style={{ color: "white" }}
-              onClick={() => deleteNote(todo.id)}
-            />
-            <FiEdit3
-              className="edit-icon"
-              style={{ color: "white", marginLeft: "30px" }}
-              onClick={() => setOnUpdate(!onUpdate)}
-            />
-          </div>
-        ) : (
-          <div className="item-edit">
-            <h5>Titulo</h5>
-            <input
-              type="text"
-              name="title"
-              onChange={(e) => setTitle(e.target.value)}
-            />
-            <h5>descricao</h5>
-            <input
-              type="text"
-              name="description"
-              onChange={(e) => setDescription(e.target.value)}
-            />
-            <FiEdit3
-              className="icon edit"
-              className="delete-icon"
-              onClick={() => editSend()}
-            />
-            <button className="edit-icon"
-              onClick={() => setOnUpdate(!onUpdate)}>
-                    Cancelar update</button>
-          </div>
-        )}
-      </>
-    );
-  };
+  return (
+    <>
+      {!onUpdate ? (
+        <div className="item-todo">
+          <h4>{todo.title}</h4>
+          <h4>{todo.description}</h4>
+          <RiDeleteBin5Line
+            className="delete-icon"
+            style={{ color: "white" }}
+            onClick={() => deleteNote(todo._id)}
+          />
+          <FiEdit3
+            className="edit-icon"
+            style={{ color: "white", marginLeft: "30px" }}
+            onClick={() => setOnUpdate(!onUpdate)}
+          />
+        </div>
+      ) : (
+        <div className="item-edit">
+          <h5>Titulo</h5>
+          <input
+            type="text"
+            name="title"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <h5>descricao</h5>
+          <input
+            type="text"
+            name="description"
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <FiEdit3
+            className="icon edit"
+            className="delete-icon"
+            onClick={() => editSend()}
+          />
+          <button className="edit-icon" onClick={() => setOnUpdate(!onUpdate)}>
+            Cancelar update
+          </button>
+        </div>
+      )}
+    </>
+  );
+};
 export default ItemTodo;
